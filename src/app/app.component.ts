4f55adc76452be943d1fbcf6d8c5e8c68f44e5f0
Uliana Radko <ulya.radko@gmail.com>
2021-04-23 09:14:00 +0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  users = [
+    {name: 'WFM 1'},
+    {name: 'WFM 2'},
+    {name: 'WFM 3'},
+    {name: 'WFM 4'},
+    {name: 'WFM 5'},
+    {name: 'WFM 6'}
+  ]
+  
+}
